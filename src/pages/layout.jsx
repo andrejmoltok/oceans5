1345b0cfd3d1e1679a364dddbc60e941a8a67f46
Csokan Pál András <csokan.andras87@protonmail.ch>
2023-10-24 23:15:59 +0200

    Add Clerk authMiddleware, useAuth in Layout

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -4,12 +4,11 @@ import styles from '@/styles/Layout.module.css';
 import { UserButton, SignOutButton, SignInButton } from '@clerk/nextjs';
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useConvexAuth, useMutation } from "convex/react";
-import { api } from "@/_generated/api";
+import { useAuth } from '@clerk/nextjs';
 
 export default function Layout({ children }) {
 
-  const { isLoading, isAuthenticated } = useConvexAuth();
+  const { isSignedIn, isLoaded, signOut, userId, sessionId } = useAuth();
 
   const router = useRouter();
 
@@ -21,8 +20,8 @@ export default function Layout({ children }) {
           <li className={styles.listitem}>Lobby</li>
           <li className={styles.listitem}>Leaderboard</li>
           <li className={styles.listitem}>How to Play</li>
-          <li className={styles.listitem}>{isAuthenticated ? <UserButton afterSignOutUrl="/" /> : 'Not logged in'}</li>
-          <li className={styles.listitem}>{!isAuthenticated && <button onClick={() => {router.push('./sign-in')}}>Sign In</button>}</li>
+          {isSignedIn ? <li className={styles.listitem}><UserButton afterSignOutUrl="/" /></li> : <li className={styles.listitem}>Not logged in</li>}
+          {!isSignedIn && <li className={styles.listitem}><button onClick={() => {router.push('./sign-in')}}>Sign In</button></li>}
         </ul>
       </div>}
       <main>{children}</main>
