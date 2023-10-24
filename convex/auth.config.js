f784b799aa62749ec90f85a8d038b39487ed3393
Csokan Pál András <csokan.andras87@protonmail.ch>
2023-10-24 21:02:39 +0200

    Add gitignore .env, convex auth.config

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://merry-cockatoo-2.clerk.accounts.dev",
+        domain: "https://assured-mink-54.clerk.accounts.dev",
         applicationID: "convex",
       },
     ]
