7e79b68e4cfc03ec2a1d4ab61ab5393fdce03e08
Csokan Pál András <csokan.andras87@protonmail.ch>
2023-10-29 04:12:34 +0100

    Add Convex Schema, Convex StoreUser mutation

---
+++ convex/schema.ts
@@ -0,0 +1,39 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  chat: defineTable({
+    //convert date-and-time input into string
+    datetime: v.string(),
+
+    //reference to users table
+    user: v.id("users"),
+
+    //the message itself that the user has sent
+    message: v.string(),
+  }),
+  users: defineTable({
+    /* Clerk JWT Claims */
+
+    //user.full_name
+    name: v.string(),
+
+    //user.primary_email_address
+    email: v.string(),
+
+    //user.image_url
+    picture: v.string(),
+
+    //user.username
+    nickname: v.string(),
+
+    //user.email_verified
+    email_verified: v.boolean(),
+
+    //user.updated_at
+    updated_at: v.string(),
+
+    //unique token
+    tokenIdentifier: v.string(),
+  }).index("by_token", ["tokenIdentifier"]),
+});
--- convex/users.js
+++
@@ -1,3 +0,0 @@
-import { query, mutation } from "./_generated/server";
-import { v } from 'convex/values';
-
