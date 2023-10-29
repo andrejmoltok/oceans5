aad1438264369652283a345a1fc742f8abf6f7a6
Csokan Pál András <csokan.andras87@protonmail.ch>
2023-10-29 08:04:32 +0100

    Revert "O5T-2 Add Sign Up button to Layout"
    This reverts commit 43956236eeb2d3cc6029e9692c20df8d8d92acf8.

--- src/pages/lobby/[[...index]].jsx
+++ src/pages/lobby/[[...index]].jsx
@@ -1,25 +1,20 @@
+'use client'
+
 import styles from '@/styles/Lobby.module.css';
-import React, { useState, useEffect, useMemo } from 'react';
-// import WebSocket from 'ws';
 
 export default function Lobby() {
-
-    
-    
 
     return (
         <>
             <div className={styles.lobby}>
                 <div className={styles.chat}>
-                    <div className={styles.messages}>
-                        
-                    </div>
+                    <div className={styles.messages}>messages</div>
                     <div className={styles.input}>
                         <div className={styles.message}>
-                            <input type="text" className={styles.inputBox} value={} onChange={(e) => (e.target.value)} />
+                            <input type="text" className={styles.inputBox} />
                         </div>
                         <div className={styles.sendButton}>
-                            <button >Send</button>
+                            <button>Send</button>
                         </div>
                     </div>
                 </div>
