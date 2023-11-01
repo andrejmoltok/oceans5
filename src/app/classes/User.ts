548b475398ea5ea77e851f93aa82bbde42ce47f5
davidpintye <53444813+davidpintye@users.noreply.github.com>
2023-11-01 19:56:53 +0100

    add userList

---
+++ src/app/classes/User.ts
@@ -0,0 +1,9 @@
+export class User {
+    userID: any;
+    userName: string;
+
+    constructor(userID: any, userName: string) {
+        this.userID = userID;
+        this.userName = userName;
+    }
+}
