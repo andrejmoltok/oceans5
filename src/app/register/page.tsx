287018e23fbe3e8d256ad027b66fe12fef508da5
Csokan Pál András <csokan.andras87@protonmail.ch>
2023-11-20 17:23:14 +0100

    Update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,27 +2,28 @@
 
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { useAuth } from '@clerk/nextjs';
 import { useMutation, useConvexAuth } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
 export default function Register() {
 
     const { isAuthenticated } = useConvexAuth();
+    const { isSignedIn } = useAuth();
 
     const router = useRouter();
 
     const storeUser = useMutation(api.users.storeUser);
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            storeUser()
-        }
-    }, [storeUser, isAuthenticated]);
+
+    if (isSignedIn && isAuthenticated) {
+        storeUser()
+        router.push('/lobby')
+    }
 
     return (
         <>
-            <div style={{ margin: 0, width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "5rem" }}>Loading...</div>
-            {router.push('/lobby')}
+            <div style={{ margin: "auto", width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "5rem" }}>Loading...</div>
         </>
     )
 }
