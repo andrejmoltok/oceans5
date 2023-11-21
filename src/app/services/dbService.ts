cefa5a2dc9398e21b94e3c091bf0a58e2ad4a242
Csokan Pál András <csokan.andras87@protonmail.ch>
2023-11-21 19:01:37 +0100

    Update dbService's new User definition

--- src/app/services/dbService.ts
+++ src/app/services/dbService.ts
@@ -1,7 +1,7 @@
 import { User } from "../classes/User"
 
 export const fetchPlayer = (user: any): User => {
-    const aUser = new User(user.id, user.username);
+    const aUser = new User(user.id, user.username, user.xp, user.level, user.rank);
     aUser.online = true;
     aUser.status = "Avaliable";
     return aUser;
